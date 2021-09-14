8feb18d281f9e1170291c6651b391ea64539becc
jason <2B3>
2021-09-14 05:09:01 -0400

    commit 283

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,49 +1,62 @@
-import { useRef } from "react";
-import { useHistory } from "react-router-dom";
+import { useRef, useState, Fragment } from "react";
+import { Prompt } from "react-router-dom";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
+  const [isEntered, setIsEntered] = useState(false);
   const authorInputRef = useRef();
   const textInputRef = useRef();
-
 
   function submitFormHandler(event) {
     // Could validate here, but I choose not to
     event.preventDefault();
     const enteredAuthor = authorInputRef.current.value;
     const enteredText = textInputRef.current.value;
-
     // Drilling upwards:
     // onAddQuote is a function from NewQuote.js that accepts the following obj of data
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
-    
   }
+  // set isEntered to true when you focus/start the form
+  // set isEntered to false when you submit the form using a button
+  const formFocusHandler = ()=> setIsEntered(true);
+  const finishFormHandler= () => setIsEntered(false);
 
+  const promptMessageFN = (location) => {
+    console.log(location);
+    return "Are you sure you wish to leave? All your entered data will be lost";
+  };
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
+    <Fragment>
+      <Prompt when={isEntered} message={promptMessageFN} /> 
+      {/* Only fire off a prompt when isEntered is true */}
+      <Card>
+        <form
+          onFocus={formFocusHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
           </div>
-        )}
-
-        <div className={classes.control}>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" rows="5" ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className="btn">Add Quote</button>
-        </div>
-      </form>
-    </Card>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={finishFormHandler} className="btn">Add Quote</button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
